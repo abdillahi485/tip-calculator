35220bc237cb8e1497edfc005fe0fc883d8c60d0
Abdillahi485 <daakircadde143@gmail.com>
2023-10-26 19:46:15 +0300

    solved tip calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,6 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let bill= 300;
+if(bill >=50 && bill<=300){
+    console.log("The bill was " + " " + bill + ", The tip was " + (300* 15/100) + " and the total value " + (300 *15/100 +bill) )
+}
