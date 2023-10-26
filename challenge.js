e044bc96c53f57759c10b801c17fd8161567ab80
Abdillahi485 <daakircadde143@gmail.com>
2023-10-26 20:01:04 +0300

    The last commit

--- challenge.js
+++ challenge.js
@@ -1,6 +1,8 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
-let bill= 300;
+let bill= 400;
 if(bill >=50 && bill<=300){
-    console.log("The bill was " + " " + bill + ", The tip was " + (300* 15/100) + " and the total value " + (300 *15/100 +bill) )
+    console.log("The bill was " + " " + bill + ", The tip was " + (bill* 15/100) + " and the total value " + (bill *15/100 +bill) )
+}else if(bill >300){
+    console.log("The bill was "  +bill +" The tip calculator was " + (bill *20/100) +" The total value "+ (bill *20/100 + bill) )
 }
